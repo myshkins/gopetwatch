4a7aca5d1fc814b0855f168d0a478b67c4339539
myshkins <alex@ak0.io>
2023-08-27 17:30:43 -0400

    add auth

--- services/temp_sender/temp_sender.py
+++ services/temp_sender/temp_sender.py
@@ -1,26 +1,37 @@
-# from sense_hat import SenseHat
-import requests
-from datetime import datetime as dt
-# import json
+# temp sender script, runs every 5 minutes via crontab
 import logging
+import os
 import random
+import requests
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+from datetime import datetime as dt
 
+
+environment = "dev"
+load_dotenv()
+if os.environ.get("LOGNAME") == "iceking":
+    from sense_hat import SenseHat
+    environment = "prod"
 
 logging.basicConfig(
     filename='temp_sender.log', encoding='utf-8', level=logging.INFO
-    )
-
-# hat = SenseHat()
-
+)
 
 def send_temp_point():
     now = str(dt.now())
-    # temp = round(hat.get_temperature(), 2)
-    # if type(temp) != float:
-    #   logging.warn("hat.get_temperature returned non-float value: %s, temp")
-    temp = random.uniform(60.0, 70.0)
+    if environment == "dev":
+        temp = random.uniform(60.0, 70.0)
+        url = "http://0.0.0.0:8081/post"
+    else:
+        hat = SenseHat()
+        temp = round(hat.get_temperature(), 2)
+        if type(temp) != float:
+            logging.warn("hat.get_temperature returned non-float value: %s, temp")
+        url = "https://gopetwatch.ak0.io/post"
     data_dict = {"temperature": round(temp, 2), "datetime": now}
-    response = requests.post(url="http://0.0.0.0:8081/post", json=data_dict)
+    basic = HTTPBasicAuth(os.environ.get("GIN_USER_ID"), os.environ.get("GIN_PW"))
+    response = requests.post(url=url, json=data_dict, auth=basic)
     response_code = response.status_code
     if response_code != 200:
         logging.warning(
